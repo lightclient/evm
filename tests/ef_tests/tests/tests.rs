238966e9f3e3284db47f54a51afb750783ee8ac1
Matt Garnett <matt.garnett@me.com>
2020-04-27 18:49:06 -0600

    reorg repo to a single package in the root

--- tests/ef_tests/tests/tests.rs
+++ tests/ef_tests/tests/tests.rs
@@ -11,25 +11,21 @@ fn vm_bitwise_operations_tests() {
     VmBitwiseOperationsHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_block_info_tests() {
     VmBlockInfoHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_environmental_info_tests() {
     VmEnvironmentalInfoHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_log_tests() {
     VmLogHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_performance_tests() {
     VmPerformanceHandler::run();
@@ -40,25 +36,21 @@ fn vm_push_dup_swap_tests() {
     VmPushDupSwapHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_random_tests() {
     VmRandomHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_sha3_tests() {
     VmSha3Handler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_system_operations_tests() {
     VmSystemOperationsHandler::run();
 }
 
-#[ignore]
 #[test]
 fn vm_tests() {
     VmTestsHandler::run();
